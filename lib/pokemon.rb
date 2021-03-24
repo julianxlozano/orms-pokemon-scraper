e036bb4ecc345f242d591c6bffee33d58de2b9cd
julianxlozano <julianxlozano@gmail.com>
2021-03-24 12:57:36 -0500

    commit tree fdf394adbf9259ea2ef9e104e1fd21a2b38e0377

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
+require 'pry'
+
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(id:,name:,type:,db:)
+        @id = id 
+        @name = name
+        @type = type
+       
+        @db = db 
+    end
+
+    def self.save(name,type,db)
+        sql = %Q(
+            INSERT INTO pokemon(name,type)
+            VALUES(?,?)
+        )
+        db.execute(sql,name,type)
+    end
+
+    def self.find(id,db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        
+        db.execute(sql,id)
+
+
+    end
+
+
 end
